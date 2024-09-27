a39e4eedb3c6c06dfba5875796a191d42327660b
yockii <xuyuqi@gmail.com>
2024-09-27 15:53:18 +0800

    chore: 调整面板大小

--- packages/plugins/func/src/MethodPanel.vue
+++ packages/plugins/func/src/MethodPanel.vue
@@ -2,7 +2,7 @@
   import type { JsMethod, Parameter } from '@ruomu-ui/types'
   import { computed, nextTick, onMounted, PropType, ref, watch } from 'vue'
   import { NIcon, NTooltip, NInput, NButtonGroup, NButton, NPopconfirm } from 'naive-ui'
-  import { Close } from '@vicons/carbon'
+  import { Close, Unknown } from '@vicons/carbon'
   import { Settings } from '@vicons/tabler'
   import { JsEditor } from '@ruomu-ui/editor'
   import ParamSettings from './ParamSettings.vue'
@@ -11,14 +11,14 @@
   const props = defineProps({
     method: {
       type: Object as PropType<JsMethod>,
-      required: true
-    }
+      required: true,
+    },
   })
   const emit = defineEmits(['close', 'confirm', 'remove'])
-  
-  
+
+
   const showNameInput = ref(false)
-  
+
   const name = ref('')
   const editName = () => {
     name.value = props.method?.name || ''
@@ -32,7 +32,7 @@
     props.method.name = name.value
     showNameInput.value = false
   }
-  
+
   const showDescriptionInput = ref(false)
   const description = ref('')
   const editDescription = () => {
@@ -43,14 +43,14 @@
     props.method.description = description.value
     showDescriptionInput.value = false
   }
-  
+
   const codeEditable = computed(() => {
     return !!props.method.name
   })
   const editableCode = ref('')
 
   const showEditor = ref(true)
-  
+
   // watch(() => props.method.code, () => {
   //   editableCode.value = `function ${props.method.name}(${params}) {\n${props.method.code || ''}\n}`
   // })
@@ -61,22 +61,22 @@
       showEditor.value = true
     })
   })
-  
+
   onMounted(() => {
-    if(props.method.name === '') {
+    if (props.method.name === '') {
       editName()
     }
     const params = paramStringArray.value.join(', ')
     editableCode.value = `function ${props.method.name}(${params}) {\n${props.method.code || ''}\n}`
   })
 
-// 参数设置
+  // 参数设置
   const paramStringArray = computed(() => {
     // return props.method.params?.map(p => `${p.name}: ${p.type}`) || []
     // return props.method.params?.map(p => `${p.name}`) || []
     return props.method.params || []
   })
-  
+
   const params = ref<Parameter[]>([])
   const paramsUpdated = (params: Parameter[]) => {
     // props.method.params 可能不存在
@@ -87,7 +87,7 @@
     params.value = props.method.params || []
     paramsDialogVisible.value = true
   }
-  
+
   const confirmAddMethod = () => {
     const code = editableCode.value
     // 去掉首尾行
@@ -96,7 +96,7 @@
     lines.pop()
     emit('confirm', lines.join('\n'))
   }
-  
+
   const showBindRelation = ref(false)
 </script>
 
@@ -104,7 +104,7 @@
   <div class="method-panel">
     <div class="flex justify-between items-center mt-4px ml-4px">
       <div v-if="showNameInput" class="flex">
-        <n-input v-model:value="name" size="small" placeholder="请设置方法名" :allow-input="nameAllowed"/>
+        <n-input v-model:value="name" size="small" placeholder="请设置方法名" :allow-input="nameAllowed" />
         <n-button-group size="small" class="ml-4px">
           <n-button type="primary" @click="confirmName">确定</n-button>
           <n-button @click="showNameInput = false">取消</n-button>
@@ -114,7 +114,7 @@
         <span class="mr-4px">方法名:</span>
         <span>{{ method.name }}</span>
         <n-tooltip>
-          <template #trigger>          
+          <template #trigger>
             <n-icon size="14" @click="editName" class="ml-8px cursor-pointer">
               <Settings />
             </n-icon>
@@ -131,7 +131,7 @@
         <span>关闭面板</span>
       </n-tooltip>
     </div>
-    
+
     <div class="mt-4px mx-4px">
       <div v-if="showDescriptionInput" class="flex">
         <n-input v-model:value="description" size="small" placeholder="请设置方法名" />
@@ -157,16 +157,41 @@
       <n-button size="small" text @click="showParamsDialog">参数设置</n-button>
       <n-button size="small" text @click="showBindRelation = true">绑定关系</n-button>
     </div>
-    <param-settings v-model:visible="paramsDialogVisible" :params="params" @update:params="paramsUpdated"/>
+    <param-settings v-model:visible="paramsDialogVisible" :params="params" @update:params="paramsUpdated" />
     <bind-relation v-if="method.id && showBindRelation" v-model:visible="showBindRelation" :method-id="method.id" />
-    
-    <p class="text-12px p-8px">说明：可直接使用state变量，作为页面数据的引用。如页面数据中定义了一个 variable1,则代码中可以直接使用state.variable1 获取其值; 项目级的store同理，可以直接引用并使用; api接口使用api引入，如定义了调用名为login的api,则可以写：api.login(config).then(res => {...})</p>
-    
+
+    <div class="flex items-center mx-4px my-4px text-12px">
+          <span>
+            支持：state、api、router、store
+          </span>
+      <n-tooltip>
+        <template #trigger>
+          <n-icon size="12" class="ml-8px cursor-pointer">
+            <Unknown />
+          </n-icon>
+        </template>
+        <p>
+          可直接使用state变量，作为页面数据的引用。如页面数据中定义了一个 variable1,则代码中可以直接使用state.variable1
+          获取其值;
+        </p>
+        <p>
+          项目级的store同理，可以直接引用并使用;
+        </p>
+        <p>
+          api接口使用api引入，如定义了调用名为login的api,则可以写：api.login(config).then(res => {...})；
+        </p>
+        <p>
+          路由跳转使用router.push(url)，如：router.push('/login')
+        </p>
+      </n-tooltip>
+    </div>
+
     <div class="code-area">
       <div v-if="!codeEditable" class="text-center text-gray-400 text-12px">请先设置方法名</div>
-      <js-editor v-if="showEditor" v-model:code="editableCode" :method-name="method.name || 'newFunction'" :params="paramStringArray" :editable="codeEditable" />
+      <js-editor v-if="showEditor" v-model:code="editableCode" :method-name="method.name || 'newFunction'"
+                 :params="paramStringArray" :editable="codeEditable" />
     </div>
-    
+
     <div class="footer">
       <div>
         <n-popconfirm @positive-click="emit('remove')" v-if="method.id">
@@ -186,19 +211,19 @@
 
 <style scoped>
   .method-panel {
-    width: 480px;
+    width: 800px;
     height: calc(100vh - 40px);
     position: absolute;
     top: -40px;
-    left: 181px;
+    left: 241px;
     background-color: #FFF;
     border-right: 1px solid #666;
-    
+
     .code-area {
       margin-top: 8px;
       height: calc(100% - 160px);
     }
-    
+
     .footer {
       z-index: 9;
       position: absolute;
